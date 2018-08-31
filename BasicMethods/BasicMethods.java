3d11438ecb50a94da769d877b24e7776bfaed21f
Kiet Ha <kaitouiet@gmail.com>
2018-08-31 08:13:18 -0400

    lesson3ish

---
+++ BasicMethods/BasicMethods.java
@@ -0,0 +1,12 @@
+public class BasicMethods {
+    public static void main(){
+        int x;
+        x = 2;
+        int y = 3;
+        
+        System.out.println(x+y); 
+    }
+    private static int add(int i, int j){
+        return i+j;
+}
+}
