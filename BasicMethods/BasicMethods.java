5a59571131505f0eb8d9e58c88c26f8ddbcf7ac2
Kiet Ha <kaitouiet@gmail.com>
2018-09-04 08:13:55 -0400

    more stuff u know?

--- BasicMethods/BasicMethods.java
+++ BasicMethods/BasicMethods.java
@@ -3,10 +3,32 @@ public class BasicMethods {
         int x;
         x = 2;
         int y = 3;
-        
-        System.out.println(x+y); 
+        int z = 10000000;
+        int nicole = 12;
+        int kiet = 2;
+
+        System.out.println (x+y); 
+        System.out.println (add(x,y,z));
+        System.out.println (substract(z,x));
+        System.out.println (multiply(nicole,x));
+        System.out.println (divide(nicole,kiet));
     }
-    private static int add(int i, int j){
-        return i+j;
+
+    private static int add(int i, int j, int k){ // inside the () is the type of data this method expects as inputs  
+        return i+j+k;
+    }
+
+    private static int substract(int a, int b){ 
+        return a-b;
+    }
+
+    private static int multiply(int q, int p){
+        return q*p;
+    }
+
+    private static int divide(int u, int y){
+        return u/y;
+    }
 }
-}
+
+/* declaring varibles */
