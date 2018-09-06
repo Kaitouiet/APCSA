76653b969d5d3a586f00b5641c59503107053b66
Kiet Ha <kaitouiet@gmail.com>
2018-09-06 07:49:48 -0400

    too used to C

--- Lesson2/HelloName.java
+++ Lesson2/HelloName.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class HelloName {
     public static void main() {
-        System.out.printf ("What is your name");
+        System.out.println ("What is your name");
         Scanner s = new Scanner(System.in);
         System.out.println ("Hello" + s.nextLine());
     }
