702b856d01e1957bc62ad65ecf702bf9c1cbb5ea
Kiet Ha <kaitouiet@gmail.com>
2018-09-13 08:01:36 -0400

    now done:)

--- EscapeSequences/EscapeSequences.java
+++ EscapeSequences/EscapeSequences.java
@@ -1,7 +1,8 @@
 public class EscapeSequences{
     public static void main(){
         System.out.println ("Descartes said: \"I think, therefore I am.\"");
-        System.out.println ("The backslash (\\) is a typographical mark \"(glyph)\" used mainly in computing.");
-}
+        System.out.println ("The backslash (\\) is a typographical mark (\"glyph\") used mainly in computing.");
+        System.out.println ("Nicole once said: \"I literally never said that.\"");
+    }
 }
         
