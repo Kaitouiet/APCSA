05110425e14f310581e482a021c72fd15dc42e81
Kiet Ha <kaitouiet@gmail.com>
2018-09-17 08:15:20 -0400

    bools eewww

---
+++ BoolTest/BoolTest.java
@@ -0,0 +1,11 @@
+
+public class BoolTest {
+    public static boolean even(int evennumbers) {
+        return (evennumbers%2)==0;
+    }
+
+    public static boolean opposite(boolean opp) {
+        return (!opp);
+    }
+}
+
