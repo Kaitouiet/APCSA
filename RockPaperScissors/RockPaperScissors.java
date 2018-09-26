1973e53b2577b9e25ab122b84762932546f29b91
Kiet Ha <kaitouiet@gmail.com>
2018-09-26 08:04:06 -0400

    wip

---
+++ RockPaperScissors/RockPaperScissors.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main (String[] arg) {
+        String[] object = {"Rock","Paper","Scissors"};
+        Scanner a = new Scanner (System.in);
+        System.out.println ("Let's play rock paper scissors!");
+        System.out.println ("Rock, Paper, or Scissors?");
+        String things = a.nextLine();
+        
+    }
+}
